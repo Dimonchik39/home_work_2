26c059b47d17c5aa65941fcce6e93d32201e61b4
Dimonchik <sudjadred@live.com>
2022-11-26 23:09:16 +0200

    add_task_1_update1

--- task_1.py
+++ task_1.py
@@ -4,9 +4,25 @@
 # 67.82 -> 23
 # (-0.56) -> 11
 
-number = input('Введите вещественное число: \n')
-summ = 0
-for digit in number:
-    if digit.isdigit():
-        summ += int(digit)
-print(summ)
+# number = input('Введите вещественное число: \n')
+# summ = 0
+# for digit in number:
+#     if digit.isdigit():
+#         summ += int(digit)
+# print(summ)
+
+
+
+try:
+    number = abs(float(input('Введите вещественное число: \n')))
+    print('Число введено верно')
+
+    str_number = str(number)
+    str_number = str_number.replace('.', '')
+    int_str = list(str_number)
+    int_num = map(int, int_str)
+    summ = sum(int_num)
+    print(f'Сумма цифр: {summ}')
+
+except ValueError:
+    print('Попробуйте заново ввести число')
